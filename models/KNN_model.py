1013d112dc011e445cdd3887959df14ceaee42f8
youssiffawzy007 <youssiffawzy200700@gmail.com>
2026-05-06 20:49:48 +0300

    Add files via upload

---
+++ models/KNN_model.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
+
+train = pd.read_csv('data/Cleaned_Train_Data.csv')
+test = pd.read_csv('data/Cleaned_Test_Data.csv')
+
+useless = ['Gender_Female', 'work_type_Govt_job', 'work_type_Private', 'work_type_Self-employed', 'work_type_children', 'smoking_status_Unknown', 'smoking_status_formerly smoked', 'smoking_status_never smoked', 'smoking_status_smokes']
+cols_to_scale = ['Age' , 'BP' , 'Cholesterol' , 'Max HR' , 'ST depression']
+
+train.drop(columns=[c for c in useless if c in train.columns], inplace=True)
+test.drop(columns=[c for c in useless if c in test.columns], inplace=True)
+
+X_train, y_train = train.drop('Heart Disease', axis=1), train['Heart Disease']
+X_test, y_test = test.drop('Heart Disease', axis=1), test['Heart Disease']
+
+
+scaler = StandardScaler()
+
+X_train[cols_to_scale] = scaler.fit_transform(X_train[cols_to_scale])
+X_test[cols_to_scale] = scaler.transform(X_test[cols_to_scale])
+
+
+model = KNeighborsClassifier()
+
+param_grid = {
+    'n_neighbors':[1, 3 , 5],
+    'metric':['euclidean' , 'manhattan' ],
+}
+
+grid = GridSearchCV(model, param_grid, cv=5, scoring='recall')
+
+grid.fit(X_train, y_train)
+
+
+best = grid.best_estimator_
+y_pred = best.predict(X_test)
+
+y_train_pred = best.predict(X_train)
+
+
+
+print("--- RANDOM FOREST ---")
+print(f"Best Hyperparameters: {grid.best_params_}")
+print(f"Accuracy:  {accuracy_score(y_test, y_pred):.4f}")
+print(f"Accuracy Train:    {accuracy_score(y_train, y_train_pred):.4f}")
+print(f"Precision: {precision_score(y_test, y_pred):.4f}")
+print(f"Recall Test:    {recall_score(y_test, y_pred):.4f}")
+print(f"F1 Score:  {f1_score(y_test, y_pred):.4f}")
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
